e44206a5974dc4792a5077fce5693e54c170a762
unknown <yeasin.mohammod018@gmail.com>
2021-07-05 17:14:10 +0600

    file up to date at Funtions folder

--- 02. Objects/objects.js
+++ 02. Objects/objects.js
@@ -1,1 +1,16 @@
 console.log("hello world")
+
+// About Object methods: 
+
+// Create an object: 
+  var mySelf= {
+    firstName :"Injamul Alam",
+    lastName : "Ismam",
+    id: 01506404,
+    fullName : function() {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+  // Display on browser data from the object: 
+  document.getElementById("methods").innerHTML=mySelf.fullName();
+  console.log(mySelf); // for testing 
--- 03. Functions/functions.js
+++ 03. Functions/functions.js
@@ -1,1 +1,5 @@
 console.log("hello world")
+
+// About Functions: 
+var myFunction = new Function("a", "b", "return a * b");
+console.log(myFunction(3, 4));
