b3da3e1ecaf7b9c2a8463efcddfef73590f41654
unknown <yeasin.mohammod018@gmail.com>
2021-07-05 17:55:03 +0600

    update BOM

--- 07. Browser BOM/browserbom.js
+++ 07. Browser BOM/browserbom.js
@@ -1,1 +1,41 @@
-console.log("hello world")
+/*
+01. Window:
+ Window Object is supported by all browsers.
+
+02. Window Screen :
+ window.screen object can be written without the window prefix.
+
+ Properties: 
+ 1. screen.width
+ 2. screen.height
+ 3. screen.availWidth
+ 4. screen.availHeight
+ 5. screen.colorDepth
+ 6. screen.pixel
+
+03. Location: 
+ Location object can be used to get the current page address (URL) and to redirect the browser to a new page.
+
+04. History:
+ History object container the browsers history.
+
+05. Navigator:
+ Navigator object contains information about the visitor's browser.
+
+06. Popup Alert:
+ JavaScript has three kind of pop up boxes.
+    1. alert box    2. confirm box    3. prompt box
+ Let's know about each one of those briefly.
+    1. Alert Box: It is often used if we want to make sure information comes through to the user.
+    2. Confirm Box: It is often used if we want the user to verify or accept something.
+    3. Prompt Box: It is often used if we want the user to input a value before entering a page.
+
+07. Timing Events:
+ The window object allows execution of code a specified time intervals. These time intervals are called 'Timing Events'.
+ They're the two key methods they use with JavaScript are 1. setTimeout and 2.setInterval. 
+
+08. Cookies: 
+ Cookies are data, stored in small text files, on your computer. 
+
+
+*/ 
