6b3e88f887b132349002ef03c41b9de8231f855c
unknown <yeasin.mohammod018@gmail.com>
2021-07-09 23:36:16 +0600

    course completed

--- 11. JavaScript vs. jQuery/jsvsjquery.js
+++ 11. JavaScript vs. jQuery/jsvsjquery.js
@@ -1,1 +1,91 @@
 console.log("hello world")
+
+/*
+
+ ** JavaScript vs jQuery **
+
+    Features:
+
+    01. Existence:
+
+        => JavaScript: It is an independent language & can exist on its own.
+        ____________________________________________________________________________________________________________
+
+        => jQuery: It is a JavaScript library. It would not have been invented had JavaScript not there. It is still dependent on JavaScript as it has to be converted to JavaScript for the browser in-built JavaScript engine to interpret and run it.
+
+    02. Language: 
+
+        => JavaScript: It is a high-level interpreted client-side scripting language. This is a combination of ECMA script and DOM (Document Object Model).
+        ____________________________________________________________________________________________________________
+
+        => jQuery: It is a light-weight JavaScript library. It has only the DOM.
+
+    03. Coding:	
+        
+        => JavaScript: It uses more lines of code as we have to write our own code.	
+        ____________________________________________________________________________________________________________        
+        => jQuery: It uses fewer lines of code than JavaScript for the same functionality as the code is already written in its library, we just have to import the library and use the relevant function/method of the library in our code.
+        
+    04. Usage:
+    
+        => JavaScript: JavaScript code is written inside the script tag in a HTML page <script> </script>.
+        ____________________________________________________________________________________________________________
+
+        => jQuery: We need to import the jQuery from CDN or from a location where the jQuery library is downloaded in order to use it. jQuery code is also written inside the script tag on the HTML page.
+
+    05. Animations:
+    	
+        => JavaScript: We can make animations in JavaScript with many lines of code. Animations are mainly done by manipulating the style of an Html page.	
+        ____________________________________________________________________________________________________________
+
+        => jQuery: We can add animation effects easily with fewer lines of code.
+
+    06. User-friendliness:	
+    
+        => JavaScript: It can be cumbersome for the developer as it can take a number of lines of code to attain a functionality.
+        ____________________________________________________________________________________________________________
+
+        => jQuery: It is more user-friendly than JavaScript with few lines of code.
+
+    07. Cross-browser compatibility:
+    
+        => JavaScript: JavaScript, we may have to deal with cross-browser compatibility by writing extra code or a workaround.
+        ____________________________________________________________________________________________________________
+
+        => jQuery: It is cross-browser compatible. We don’t need to worry about writing any workaround or extra code to make our code compatible to a browser.
+    
+    08. Performance:
+    
+        => JavaScript: Pure JavaScript can be faster for DOM selection/manipulation than jQuery as JavaScript is directly processed by the browser.
+        ____________________________________________________________________________________________________________
+        
+        =>jQuery: It has to be converted into JavaScript to make it run in a browser.
+    
+    09. Event handling, Interactivity, and Ajax calls:
+        
+        => JavaScript: All these can be done in JavaScript but we may have to write many lines of code.	
+        ____________________________________________________________________________________________________________
+
+        =>jQuery:All these can be done easily with jQuery with fewer lines of code. It is easier in jQuery to add interactivity, animations and also make ajax calls as the functions are already pre-defined in the library. We just use those functions in our code at the required places.
+
+    10. Verbosity:
+    	
+        => JavaScript: It is verbose as one has to write many lines of code for a functionality.
+        ____________________________________________________________________________________________________________
+
+        => jQuery: It is concise and uses fewer lines of code, sometimes only one line of code.
+
+    11. Size and Weight:
+
+    	=> JavaScript: Being a language, it is heavier than jQuery.	
+        ____________________________________________________________________________________________________________
+
+        => jQuery: Being a library, it is lightweight. It has a minified version of its code which makes it light-weight.
+    
+    12. Reusability and Maintainability:	
+    
+        => JavaScript: JavaScript code can be verbose and therefore can be difficult to maintain and reuse.
+        ____________________________________________________________________________________________________________
+        
+        => jQuery: With fewer lines of code, jQuery is more maintainable as we just have to call the predefined functions in the jQuery library in our code. This also makes us to easily reuse jQuery functions at different places in the code.
+*/
